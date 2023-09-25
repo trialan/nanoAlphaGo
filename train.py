46bd941f82a7210f1b5fb04979d39ad4cdc55388
Thomas Rialan <thomasrialan@gmail.com>
2023-09-25 16:49:04 +0200

    minor speedups

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ from nanoAlphaGo.rl.eval import performance_against_random_policy
 from nanoAlphaGo.game.board import GoBoard
 from nanoAlphaGo.config import WHITE
 
-EPOCHS = 5
+EPOCHS = 1
 
 if __name__ == '__main__':
     board = GoBoard()
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     value_network = ValueNN(board)
 
     for _ in range(EPOCHS):
-        ppo_train(policy_network, value_network, n_loops=10)
+        ppo_train(policy_network, value_network, n_loops=5)
         performance = performance_against_random_policy(policy_network,
                                                         n_games=2)
         print(performance)
